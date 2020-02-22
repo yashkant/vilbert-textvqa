7b8128a9dcb5d3bba2216a8ca78b046e6a7ce0fb
Yash Kant <ysh.kant@gmail.com>
2020-02-22 00:00:07 -0500

    Add STVQA vocab and structure configs

--- vilbert/datasets/textvqa_processors.py
+++ vilbert/datasets/textvqa_processors.py
@@ -859,6 +859,7 @@ class M4CAnswerProcessor:
         vocab4k_filtered = "/srv/share/ykant3/pythia/vocabs/answers_textvqa_more_than_1_m4c_no_dups_filtered.txt"
         vocab_none = "/srv/share/ykant3/pythia/vocabs/vocab_none.txt"
         vocab4k_latest = "/srv/share/ykant3/pythia/vocabs/answer_vocab_textvqa_4k_filtered.txt"
+        vocab5k_stvqa = "/srv/share/ykant3/m4c-release/data/m4c_vocabs/stvqa/fixed_answer_vocab_stvqa_5k.txt"
 
         if config.vocab_type == "5k":
             self.answer_vocab = VocabDict(vocab5k, *args, **kwargs)
@@ -870,6 +871,8 @@ class M4CAnswerProcessor:
             self.answer_vocab = VocabDict(vocab_none, *args, **kwargs)
         elif config.vocab_type == "4k_latest":
             self.answer_vocab = VocabDict(vocab4k_latest, *args, **kwargs)
+        elif config.vocab_type == "5k_stvqa":
+            self.answer_vocab = VocabDict(vocab5k_stvqa, *args, **kwargs)
         else:
             raise ValueError
 
