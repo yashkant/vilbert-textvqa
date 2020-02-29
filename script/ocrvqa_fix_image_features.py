3839c360c242b455f1b94f2fe1b5dbe295d8ada6
Yash Kant <ysh.kant@gmail.com>
2020-02-29 15:36:36 -0500

    Fix training of VQA

--- script/ocrvqa_fix_image_features.py
+++ script/ocrvqa_fix_image_features.py
@@ -20,12 +20,48 @@ from maskrcnn_benchmark.modeling.detector import build_detection_model
 from maskrcnn_benchmark.structures.image_list import to_image_list
 from maskrcnn_benchmark.utils.model_serialization import load_state_dict
 from tqdm import tqdm
+import os
+
+from typing import List
+import csv
+import h5py
+import numpy as np
+import copy
+import pickle
+import lmdb  # install lmdb by "pip install lmdb"
+import base64
+import pdb
 
 
-# image_folders
+IMDB_OCRVQA_SPLITS = [
+    "/srv/share/ykant3/ocr-vqa/intermediate/ocrvqa_train.npy",
+    "/srv/share/ykant3/ocr-vqa/intermediate/ocrvqa_val.npy",
+    "/srv/share/ykant3/ocr-vqa/intermediate/ocrvqa_test.npy",
+]
+
 IMAGES_OCRVQA = "/srv/share/ykant3/ocr-vqa/images/"
-OBJ_FEATURES_OCRVQA = "/srv/share/ykant3/ocr-vqa/features/obj/"
 
+OCR_FEATURES_OCRVQA = "/srv/share/ykant3/ocr-vqa/features/"
+
+def preprocess_ocr_tokens(info, prefix, suffix):
+    """CRUX: given an entry return list of bboxes [x1,y1,x2,y2] and tokens"""
+    height = info['image_height']
+    width = info['image_width']
+    ocr_info = info['{}ocr_info{}'.format(prefix, suffix)]
+
+    boxes = np.zeros((len(ocr_info), 4), np.float32)
+    tokens = [None]*len(ocr_info)
+    for idx, entry in enumerate(ocr_info):
+        tokens[idx] = entry['word']
+        box = entry['bounding_box']
+        x1 = box['top_left_x'] * width
+        y1 = box['top_left_y'] * height
+        w = box['width'] * width
+        h = box['height'] * height
+        x2 = x1 + w
+        y2 = y1 + h
+        boxes[idx] = [x1, y1, x2, y2]
+    return boxes, tokens
 
 class FeatureExtractor:
     MAX_SIZE = 1333
@@ -34,8 +70,6 @@ class FeatureExtractor:
     def __init__(self):
         self.args = self.get_parser().parse_args()
         self.detection_model = self._build_detection_model()
-
-        os.makedirs(self.args.output_folder, exist_ok=True)
 
     def get_parser(self):
         parser = argparse.ArgumentParser()
@@ -52,14 +86,6 @@ class FeatureExtractor:
             default=100,
             help="Number of features to extract.",
         )
-        parser.add_argument(
-            "--output_folder", type=str, default="/srv/share/ykant3/", help="Output folder"
-        )
-
-        parser.add_argument(
-            "--image_dir", type=str, default=None
-        )
-
         parser.add_argument(
             "--feature_name",
             type=str,
@@ -82,6 +108,26 @@ class FeatureExtractor:
         )
         return parser
 
+    def preprocess_ocr_tokens(info, prefix, suffix):
+        """CRUX: given an entry return list of bboxes [x1,y1,x2,y2] and tokens"""
+        height = info['image_height']
+        width = info['image_width']
+        ocr_info = info['{}ocr_info{}'.format(prefix, suffix)]
+
+        boxes = np.zeros((len(ocr_info), 4), np.float32)
+        tokens = [None] * len(ocr_info)
+        for idx, entry in enumerate(ocr_info):
+            tokens[idx] = entry['word']
+            box = entry['bounding_box']
+            x1 = box['top_left_x'] * width
+            y1 = box['top_left_y'] * height
+            w = box['width'] * width
+            h = box['height'] * height
+            x2 = x1 + w
+            y2 = y1 + h
+            boxes[idx] = [x1, y1, x2, y2]
+        return boxes, tokens
+
     def _build_detection_model(self):
         cfg.merge_from_file(self.args.config_file)
         cfg.freeze()
@@ -95,167 +141,142 @@ class FeatureExtractor:
         model.eval()
         return model
 
-    def _image_transform(self, path):
-        img = Image.open(path)
+    def _image_transform(self, image_path):
+        """Handles 2-dim and n-dim images. Resize between (800, 1333) and output scale"""
+        img = Image.open(image_path)
         im = np.array(img).astype(np.float32)
-        # IndexError: too many indices for array, grayscale images
-        if len(im.shape) < 3:
-            im = np.repeat(im[:, :, np.newaxis], 3, axis=2)
-        # Handle 4-channel images
+        if im.ndim == 2:
+            im = np.tile(im[:, :, None], (1, 1, 3))
         if im.shape[2] > 3:
             im = im[:, :, :3]
         im = im[:, :, ::-1]
         im -= np.array([102.9801, 115.9465, 122.7717])
         im_shape = im.shape
-        im_height = im_shape[0]
-        im_width = im_shape[1]
         im_size_min = np.min(im_shape[0:2])
         im_size_max = np.max(im_shape[0:2])
-
-        # Scale based on minimum size
-        im_scale = self.MIN_SIZE / im_size_min
-
+        im_scale = float(800) / float(im_size_min)
         # Prevent the biggest axis from being more than max_size
-        # If bigger, scale it down
-        if np.round(im_scale * im_size_max) > self.MAX_SIZE:
-            im_scale = self.MAX_SIZE / im_size_max
-
+        if np.round(im_scale * im_size_max) > 1333:
+            im_scale = float(1333) / float(im_size_max)
         im = cv2.resize(
-            im, None, None, fx=im_scale, fy=im_scale, interpolation=cv2.INTER_LINEAR
+            im,
+            None,
+            None,
+            fx=im_scale,
+            fy=im_scale,
+            interpolation=cv2.INTER_LINEAR
         )
         img = torch.from_numpy(im).permute(2, 0, 1)
+        return img, im_scale, im_shape
 
-        im_info = {"width": im_width, "height": im_height}
+    def get_detectron_features(self, image_path, input_boxes):
+        # img_tensor, im_scales, im_infos = [], [], []
+        im, im_scale, im_shape = self._image_transform(image_path)
 
-        return img, im_scale, im_info
+        if input_boxes is not None:
+            if isinstance(input_boxes, np.ndarray):
+                input_boxes = torch.from_numpy(input_boxes.copy()).to("cuda")
+            input_boxes *= im_scale
+            input_boxes = [input_boxes]
 
-    def _process_feature_extraction(
-        self, output, im_scales, im_infos, feature_name="fc6", conf_thresh=0
-    ):
-        batch_size = len(output[0]["proposals"])
-        n_boxes_per_image = [len(boxes) for boxes in output[0]["proposals"]]
-        score_list = output[0]["scores"].split(n_boxes_per_image)
-        score_list = [torch.nn.functional.softmax(x, -1) for x in score_list]
-        feats = output[0][feature_name].split(n_boxes_per_image)
-        cur_device = score_list[0].device
-
-        feat_list = []
-        info_list = []
-
-        for i in range(batch_size):
-            dets = output[0]["proposals"][i].bbox / im_scales[i]
-            scores = score_list[i]
-            max_conf = torch.zeros((scores.shape[0])).to(cur_device)
-            conf_thresh_tensor = torch.full_like(max_conf, conf_thresh)
-            start_index = 1
-            # Column 0 of the scores matrix is for the background class
-            if self.args.background:
-                start_index = 0
-            for cls_ind in range(start_index, scores.shape[1]):
-                cls_scores = scores[:, cls_ind]
-                keep = nms(dets, cls_scores, 0.5)
-                max_conf[keep] = torch.where(
-                    # Better than max one till now and minimally greater than conf_thresh
-                    (cls_scores[keep] > max_conf[keep])
-                    & (cls_scores[keep] > conf_thresh_tensor[keep]),
-                    cls_scores[keep],
-                    max_conf[keep],
-                )
-
-            sorted_scores, sorted_indices = torch.sort(max_conf, descending=True)
-            num_boxes = (sorted_scores[: self.args.num_features] != 0).sum()
-            keep_boxes = sorted_indices[: self.args.num_features]
-            feat_list.append(feats[i][keep_boxes])
-            bbox = output[0]["proposals"][i][keep_boxes].bbox / im_scales[i]
-            # Predict the class label using the scores
-            objects = torch.argmax(scores[keep_boxes][start_index:], dim=1)
-            cls_prob = torch.max(scores[keep_boxes][start_index:], dim=1)
-
-            info_list.append(
-                {
-                    "bbox": bbox.cpu().numpy(),
-                    "num_boxes": num_boxes.item(),
-                    "objects": objects.cpu().numpy(),
-                    "image_width": im_infos[i]["width"],
-                    "image_height": im_infos[i]["height"],
-                    "cls_prob": scores[keep_boxes].cpu().numpy(),
-                }
-            )
-
-        return feat_list, info_list
-
-    def get_detectron_features(self, image_paths):
-        img_tensor, im_scales, im_infos = [], [], []
-
-        for image_path in image_paths:
-            im, im_scale, im_info = self._image_transform(image_path)
-            img_tensor.append(im)
-            im_scales.append(im_scale)
-            im_infos.append(im_info)
-
-        # Image dimensions should be divisible by 32, to allow convolutions
-        # in detector to work
+        img_tensor, im_scales = [im], [im_scale]
+        # im.shape: (C,H,W)
+        # batches the images and decides the max_size
         current_img_list = to_image_list(img_tensor, size_divisible=32)
-        current_img_list = current_img_list.to("cuda")
+        current_img_list = current_img_list.to('cuda')
 
         with torch.no_grad():
-            output = self.detection_model(current_img_list)
+            output = self.detection_model(current_img_list, input_boxes=input_boxes)
 
-        feat_list = self._process_feature_extraction(
-            output,
-            im_scales,
-            im_infos,
-            self.args.feature_name,
-            self.args.confidence_threshold,
-        )
+        feat = output[0][self.args.feature_name].cpu().numpy()
+        bbox = output[0]["proposals"][0].bbox.cpu().numpy() / im_scale
 
-        return feat_list
+        return feat, bbox
 
-    def _save_feature(self, save_path, feature, info):
-        file_base_name = save_path.split(".")[0]
-        info["image_id"] = os.path.split(file_base_name)[-1]
-        file_base_name = file_base_name + ".npy"
-        info["features"] = feature.cpu().numpy()
-        np.save(file_base_name, info)
+    def _chunks(self, array, chunk_size):
+        for i in range(0, len(array), chunk_size):
+            yield array[i : i + chunk_size]
 
-    def extract_features(self, image_dir, save_dir):
-        # paths to all the items images
-        files = glob.glob(image_dir + "/*", recursive=True)
-
+    def extract_features(self, imdb_data, image_ids, save_dir, image_dir):
+        prefix, suffix = "google_", "_filtered"
         import pdb
         pdb.set_trace()
 
-        # find remaining files that are extracted yet!
-        remain_files = []
-        for file in tqdm(files, desc="Counting files"):
-            feature_file = os.path.join(save_dir, os.path.split(file)[-1]).split(".")[0] + ".npy"
-            try:
-                assert os.path.exists(feature_file)
-                x = np.load(feature_file, allow_pickle=True)
-            except:
-                print(f"Adding file: {file}")
-                remain_files.append(file)
-
+        image_ids_dict = {}
+        for image_id in image_ids:
+            image_ids_dict[image_id.decode()] = None
+        
         import pdb
         pdb.set_trace()
 
-        assert os.path.exists(image_dir)
-        assert len(files) > 0
-        for file in tqdm(remain_files):
+        image_paths = []
+        for instance in tqdm(imdb_data[1:]):
+            image_id = instance["image_id"]
+            if image_id not in image_ids_dict:
+                image_path = os.path.join(IMAGES_OCRVQA, instance["image_path"])
+                image_paths.append(image_path)
+        import pdb
+        pdb.set_trace()
+
+
+        for image_path in tqdm(image_paths):
+            image_paths.append(image_path)
+            assert os.path.exists(image_path)
+            image_w, image_h = Image.open(image_path).size
+            instance["image_width"], instance["image_height"] = image_w, image_h
+            ocr_boxes, ocr_tokens = preprocess_ocr_tokens(instance, prefix, suffix)
             try:
-                features, infos = self.get_detectron_features([file])
-                save_path = file.replace(image_dir, save_dir)
+                if len(ocr_boxes) > 0:
+                    extracted_feat, _ = self.get_detectron_features(image_path, input_boxes=ocr_boxes)
+                else:
+                    extracted_feat = np.zeros((0, 2048), np.float32)
+
+                assert len(extracted_feat) == len(ocr_boxes)
+                save_data = {
+                    "ocr_boxes": ocr_boxes,
+                    "ocr_tokens": ocr_tokens,
+                    "features": extracted_feat
+                }
+
                 _save_dir = os.path.split(save_path)[0]
-                assert os.path.exists(_save_dir)
-                self._save_feature(save_path, features[0], infos[0])
+                if not os.path.exists(_save_dir):
+                    os.makedirs(_save_dir)
+                    print(f"Creating Dir: {_save_dir}")
+
+                np.save(
+                    save_path,
+                    save_data
+                )
             except BaseException:
-                import pdb
-                pdb.set_trace()
-                print("Couldn't extract from: ", file)
+                print("Couldn't extract from: ", instance["image_path"])
 
 
 if __name__ == "__main__":
     feature_extractor = FeatureExtractor()
-    # Todo: Store fc7 weights from here.
-    print(f"Extracting from: {IMAGES_OCRVQA} \nSaving to: {OBJ_FEATURES_OCRVQA}")
-    feature_extractor.extract_features(IMAGES_OCRVQA, OBJ_FEATURES_OCRVQA)
+    # with h5py.File(self.features_h5path, "r", libver='latest', swmr=True) as features_h5:
+    # self._image_ids = list(features_h5["image_ids"])
+    # If not loaded in memory, then list of None.
+    env = lmdb.open(
+        "/srv/share/ykant3/ocr-vqa/features/lmdbs/ocrvqa_ocr.lmdb",
+        max_readers=1,
+        readonly=True,
+        lock=False,
+        readahead=False,
+        meminit=False,
+    )
+
+    with env.begin(write=False) as txn:
+        _image_ids = pickle.loads(txn.get("keys".encode()))
+        id = b'/srv/share/ykant3/ocr-vqa/features/ocr/1599215322.npy'
+        import pdb
+        pdb.set_trace()
+        item = pickle.loads(txn.get(id))
+
+
+    print(f"Extracting for: {IMDB_OCRVQA_SPLITS}")
+    for imdb_file in IMDB_OCRVQA_SPLITS:
+        print(f"Reading from: {imdb_file} \nSaving to: {OCR_FEATURES_OCRVQA}")
+        imdb_data = np.load(imdb_file, allow_pickle=True)
+        feature_extractor.extract_features(imdb_data, image_ids, OCR_FEATURES_OCRVQA, IMAGES_OCRVQA)
+
+    # Todo: Check from LMDB the missing keys and only replace and handle them! 
