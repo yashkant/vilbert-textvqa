0cc7259c85858c3c0c0b38fd3f027283b4f749c5
Yash Kant <ysh.kant@gmail.com>
2020-02-29 06:05:26 -0500

    Fix logging bug

--- vilbert/utils.py
+++ vilbert/utils.py
@@ -268,7 +268,7 @@ class tbLogger(object):
 
     def step_val(self, epochId, loss, score, task_id, batch_size, split):
         self.task_loss_val[task_id] += loss * batch_size
-        self.task_score_val[task_id] += score
+        self.task_score_val[task_id] += score*batch_size
         self.task_step_val[task_id] += self.gradient_accumulation_steps
         self.task_datasize_val[task_id] += batch_size
 
