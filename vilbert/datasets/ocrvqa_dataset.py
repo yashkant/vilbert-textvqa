aad564e2304fb5e59f74f857f830b303ab747b90
Yash Kant <ysh.kant@gmail.com>
2020-02-29 19:12:22 -0500

    Add ocr-vqa configs

--- vilbert/datasets/ocrvqa_dataset.py
+++ vilbert/datasets/ocrvqa_dataset.py
@@ -154,12 +154,13 @@ class OCRVQADataset(TextVQADataset):
                 task + "_" + split + "_" + str(max_seq_length) + clean_train + f"_vocab_type{self.vocab_type}"
                 + f"_dynamic_{self.dynamic_sampling}" + ".pkl",
             )
+        logger.info(f"Cache Name:  {cache_path}")
 
         if self.heads_type != "none" or self.randomize > 0:
             spatial_cache_path = cache_path.split(".")[0]
             spatial_cache_path = spatial_cache_path + f"_spatials" + ".pkl"
+            logger.info(f"Spatial Cache Name:  {spatial_cache_path}")
 
-        logger.info(f"Cache Name:  {cache_path}")
 
         if not os.path.exists(cache_path) or self.debug:
             # Initialize Processors
@@ -208,15 +209,21 @@ class OCRVQADataset(TextVQADataset):
         for entry in tqdm(self.entries, desc="Reading Entries"):
             if entry["image_id"] in image_id_obj_list:
                 continue
-            # Adding spatial graph matrix
-            obj_features, obj_num_boxes, obj_bboxes, _ = self._image_features_reader[entry["image_id"]]
+
+            obj_features, obj_num_boxes, obj_bboxes, _ = self.obj_features_reader[entry["image_id"]]
             obj_features, obj_num_boxes, obj_bboxes = obj_features[1:], obj_num_boxes - 1, obj_bboxes[1:]
             _, _, pad_obj_bboxes = self._pad_features(
-                obj_features, obj_bboxes, obj_num_boxes, self._max_region_num, tensorize=False
+                obj_features, obj_bboxes, obj_num_boxes, self.max_obj_num, tensorize=False
             )
-            pad_obj_bboxes = pad_obj_bboxes[:, :-1]
+            ocr_features, ocr_num_boxes, ocr_bboxes, _ = self.ocr_features_reader[entry["image_id"]]
+            ocr_features, ocr_num_boxes, ocr_bboxes = ocr_features[1:], ocr_num_boxes - 1, ocr_bboxes[1:]
+            _, _, pad_ocr_bboxes = self._pad_features(
+                ocr_features, ocr_bboxes, ocr_num_boxes, self.max_ocr_num, tensorize=False
+            )
+
             # Append bboxes to the list
-            image_id_obj_list[entry["image_id"]] = pad_obj_bboxes
+            pad_obj_ocr_bboxes = np.concatenate([pad_obj_bboxes[:, :-1], pad_ocr_bboxes[:, :-1]], axis=0)
+            image_id_obj_list[entry["image_id"]] = pad_obj_ocr_bboxes
 
         image_ids = []
         obj_lists = []
@@ -254,3 +261,139 @@ class OCRVQADataset(TextVQADataset):
         adj_matrix_random1, adj_matrix_random3 = random_spatial_processor(pad_obj_bboxes)
         return adj_matrix, adj_matrix_share3_1, adj_matrix_share3_2, adj_matrix_random1, adj_matrix_random3
 
+
+    def __getitem__(self, index):
+        """
+        1. Get image-features/bboxes and image mask (as nump-arrays), tensorize them.
+        2. Get question, input_mask, segment_ids and coattention mask
+        3. Build target (vocab-dim) with VQA scores scattered at label-indices
+        4. Return
+        """
+        entry = self.entries[index]
+        image_id = entry["image_id"]
+
+        # add object-features and bounding boxes
+        obj_features, obj_num_boxes, obj_bboxes, _ = self.obj_features_reader[image_id]
+        # remove avg-features
+        obj_features, obj_num_boxes, obj_bboxes = obj_features[1:], obj_num_boxes - 1, obj_bboxes[1:]
+        pad_obj_features, pad_obj_mask, pad_obj_bboxes = self._pad_features(
+            obj_features, obj_bboxes, obj_num_boxes, self.max_obj_num
+        )
+
+        # add ocr-features and bounding boxes
+        ocr_features, ocr_num_boxes, ocr_bboxes, _ = self.ocr_features_reader[image_id]
+        # remove avg-features
+        ocr_features, ocr_num_boxes, ocr_bboxes = ocr_features[1:], ocr_num_boxes - 1, ocr_bboxes[1:]
+        pad_ocr_features, pad_ocr_mask, pad_ocr_bboxes = self._pad_features(
+            ocr_features, ocr_bboxes, ocr_num_boxes, self.max_ocr_num
+        )
+
+        co_attention_mask = torch.cat((pad_obj_mask, pad_ocr_mask))
+        co_attention_mask = co_attention_mask.unsqueeze(1).repeat(1, self._max_seq_length)
+        segment_ids = torch.zeros_like(entry["question_mask"])
+
+        item = edict({
+            "pad_obj_features": pad_obj_features,
+            "pad_obj_mask": pad_obj_mask,
+            "pad_obj_bboxes": pad_obj_bboxes,
+            "pad_ocr_features": pad_ocr_features,
+            "pad_ocr_mask": pad_ocr_mask,
+            "pad_ocr_bboxes": pad_ocr_bboxes,
+            "segment_ids": segment_ids
+        })
+
+        item["co_attention_mask"] = co_attention_mask
+
+        # process answers (dynamic sampling)
+        if self.clean_answers:
+            cleaned_answers = [Processors.word_cleaner(word) for word in entry["answers"]]
+        else:
+            cleaned_answers = entry["answers"]
+
+        cleaned_ocr_tokens = entry["cleaned_ocr_tokens"]
+        processed_answers = self.processors.answer_processor({
+            "answers": cleaned_answers,
+            "context_tokens": cleaned_ocr_tokens,
+        })
+        entry.update(processed_answers)
+
+
+        if self.heads_type in ["mix", "share3", "quad4"] or self.randomize > 0:
+            spatials = self.spatials[image_id]
+            # In the first iteration expand all the spatial relation matrices
+
+            if not isinstance(spatials["spatial_adj_matrix"], torch.Tensor):
+                if self.randomize > 0:
+                    rows, cols, slices = spatials["spatial_adj_matrix"].shape
+                    assert slices == self.randomize
+                    adj_matrices = []
+
+                    # Expand each slice
+                    for slice_idx in range(slices):
+                        adj_matrix_slice = torch_broadcast_adj_matrix(
+                            torch.from_numpy(spatials["spatial_adj_matrix"][:, :, slice_idx]),
+                            label_num=12
+                        )
+                        adj_matrices.append(adj_matrix_slice)
+
+                    # Aggregate each slice
+                    spatials["spatial_adj_matrix"] = adj_matrices[0]
+                    for matrix_slice in adj_matrices[1:]:
+                        spatials["spatial_adj_matrix"] = torch.max(spatials["spatial_adj_matrix"], matrix_slice)
+
+                    entry["spatial_loss_mask"] = entry["spatial_ocr_relations"] = None
+                else:
+                    spatials["spatial_adj_matrix"] = torch_broadcast_adj_matrix(
+                        torch.from_numpy(spatials["spatial_adj_matrix"]),
+                        label_num=12
+                    )
+
+                if self.heads_type == "mix":
+                    spatial_adj_matrix_share3_1 = torch_broadcast_adj_matrix(
+                        torch.from_numpy(spatials["spatial_adj_matrix_share3_1"]),
+                        label_num=12
+                    )
+                    spatial_adj_matrix_share3_2 = torch_broadcast_adj_matrix(
+                        torch.from_numpy(spatials["spatial_adj_matrix_share3_2"]),
+                        label_num=12
+                    )
+                    spatials["spatial_adj_matrix_share3"] = torch.max(spatials["spatial_adj_matrix"],
+                                                                   spatial_adj_matrix_share3_1)
+                    spatials["spatial_adj_matrix_share3"] = torch.max(spatials["spatial_adj_matrix"],
+                                                                   spatial_adj_matrix_share3_2)
+            else:
+                try:
+                    assert len(spatials["spatial_adj_matrix"].shape) == 3
+                except:
+                    import pdb
+                    pdb.set_trace()
+            item.update(spatials)
+        item.update(entry)
+
+
+        # remove unwanted keys
+        unwanted_keys = ['spatial_adj_matrix_share3_1',
+                         'spatial_adj_matrix_share3_2',
+                         'spatial_adj_matrix_random1',
+                         'spatial_adj_matrix_random3',
+                         'cleaned_ocr_tokens',
+                         'image_id',
+                         'image_path']
+
+        if self.heads_type == "share3":
+            unwanted_keys.append("spatial_adj_matrix_quad4")
+
+        for key in unwanted_keys:
+            if key in item:
+                item.pop(key, None)
+
+        # Collate Function doesn't work correctly with lists
+        for key, value in item.items():
+            if not isinstance(value, torch.Tensor):
+                try:
+                    item[key] = enc_obj2bytes(value)
+                except:
+                    import pdb
+                    pdb.set_trace()
+
+        return item
