Fix bug in masking

evaluate_textvqa.py
@@ -110,6 +110,10 @@ def parse_args():
         "--split", default="val", type=str, help="Which split do you want to evaluate"
     )
 
+    parser.add_argument(
+        "--use_share2", default=False, type=bool, help="Which split do you want to evaluate"
+    )
+
     command_args = parser.parse_args()
 
     # if default value is going to change, add those in command line arguments.
@@ -120,7 +124,7 @@ def parse_args():
         'do_lower_case': True,
         'in_memory': True,
         'gradient_accumulation_steps': 1,
-        'num_workers': 0,
+        'num_workers': 8,
         'local_rank': -1,
         'clean_train_sets': False,
         'num_train_epochs': 100,
@@ -151,6 +155,12 @@ def load_model():
     elif model_type == "m4c_spatial":
         logger.info("Using M4C-Spatial model")
         from vilbert.m4c_spatial import BertConfig, M4C
+    elif model_type == "m4c_topk":
+        logger.info("Using M4C-Topk model")
+        from vilbert.m4c_topk import BertConfig, M4C
+    elif model_type == "m4c_topk_20x":
+        logger.info("Using M4C-Topk model")
+        from vilbert.m4c_topk_20x import BertConfig, M4C
     elif model_type == "m4c_spatial_que_cond":
         logger.info("Using M4C-Spatial Question Cond. model")
         from vilbert.m4c_spatial_que_cond import BertConfig, M4C
@@ -286,7 +296,7 @@ def evaluate(
             sys.stdout.write("%d/%d\r" % (i, len(task_dataloader_val[task_id])))
             sys.stdout.flush()
 
-            if args.short_eval and i == 3:
+            if args.short_eval and i == 1:
                 break
 
     return predictions
@@ -301,7 +311,6 @@ def vqa_calculate(batch_dict):
 
     predictions = []
 
-    # TODO: This is a single list - remove for loop
     for idx, question_id in enumerate([batch_dict["question_id"]]):
         context_tokens = ocr_tokens_enc[idx]
         answer_words = []
@@ -381,7 +390,12 @@ def anls_calculate(batch_dict):
             "topkscores": topkscores
         })
 
-    accuracy, pred_scores = anls_evaluator.eval_pred_list(predictions)
+    try:
+        accuracy, pred_scores = anls_evaluator.eval_pred_list(predictions)
+    except:
+        import pdb
+        pdb.set_trace()
+
     return {
         'question_id': predictions[0]['question_id'],
         'accuracy': accuracy,
@@ -457,7 +471,11 @@ def main():
     registry['vocab'] = vocab
 
     # Load Dataset
-    return_tuple = LoadDatasets(args, task_cfg, args.tasks.split("-"), split=args.split, only_val=True)
+    return_tuple = LoadDatasets(args, task_cfg, args.tasks.split("-"),
+                                split=args.split,
+                                only_val=True,
+                                test_val_bs=64,
+                                test_val_workers=8)
 
     if "test" in args.split:
         (
@@ -480,7 +498,7 @@ def main():
             task_datasets_val,
             task_dataloader_train,
             task_dataloader_val
-        ) = tuple
+        ) = return_tuple
 
     task_losses = LoadLosses(args, task_cfg, args.tasks.split("-"))
 
@@ -494,8 +512,6 @@ def main():
     # pd.to_pickle(stvqa_eval_df, eval_df_path_stvqa)
     # pd.to_pickle(stvqa_eval_df_test, eval_df_path_stvqa_test)
 
-    import pdb
-    pdb.set_trace()
 
     if task_cfg["TASK19"]["val_on"][0] == "textvqa":
         path = eval_df_path_tvqa
@@ -516,13 +532,13 @@ def main():
 
     for split in ["test", "val"]:
 
+        if split not in args.split:
+            continue
+
         if "test" in split:
             dataloader = task_dataloader_test
         elif "val" in split:
             dataloader = task_dataloader_val
-
-        import pdb
-        pdb.set_trace()
 
         logger.info(f"Processing split: {split}")
         if split in args.split:
@@ -537,36 +553,44 @@ def main():
                 model,
                 task_losses
             )
+
             curr_score['complete_seqs'] = np.concatenate(
                 [x.reshape(-1, 12) for x in curr_score['complete_seqs']], axis=0
             ).tolist()
             curr_score['topkscores'] = np.concatenate(curr_score['topkscores'], axis=0).tolist()
             curr_score['question_id'] = np.concatenate(curr_score['question_id'], axis=0).tolist()
 
+            if 'answers' not in eval_df:
+                eval_df["answers"] = [["none"]*10]*len((eval_df["question_id"]))
+
             # Compute VQA Accuracies
             results_df = pd.DataFrame.from_dict(curr_score, orient='columns')
 
             # Get both type of accuracies!
             accuracies = evaluate_predictions(eval_df, results_df, acc_type="vqa")
-            logger.info(
-                "{} Accuracy: {} for {} questions, split {}, dataset {}".format(
-                    "vqa",
-                    accuracies['vqa_accuracy'],
-                    accuracies['accuracies_df'].shape,
-                    args.split,
-                    task_cfg["TASK19"]["val_on"][0])
-            )
 
-            accuracies = evaluate_predictions(eval_df, results_df, acc_type="anls")
-            logger.info(
-                "{} Accuracy: {} for {} questions, split {}, dataset {}".format(
-                    "anls",
-                    accuracies['vqa_accuracy'],
-                    accuracies['accuracies_df'].shape,
-                    args.split,
-                    task_cfg["TASK19"]["val_on"][0])
-            )
-            evalai_file = os.path.join(os.path.dirname(args.model_ckpt),'{}_evalai_beam_{}.json'.format(split, args.beam_size))
+            if "test" not in split:
+                logger.info(
+                    "{} Accuracy: {} for {} questions, split {}, dataset {}".format(
+                        "vqa",
+                        accuracies['vqa_accuracy'],
+                        accuracies['accuracies_df'].shape,
+                        args.split,
+                        task_cfg["TASK19"]["val_on"][0])
+                )
+
+                accuracies = evaluate_predictions(eval_df, results_df, acc_type="anls")
+                logger.info(
+                    "{} Accuracy: {} for {} questions, split {}, dataset {}".format(
+                        "anls",
+                        accuracies['vqa_accuracy'],
+                        accuracies['accuracies_df'].shape,
+                        args.split,
+                        task_cfg["TASK19"]["val_on"][0])
+                )
+
+            evalai_file = os.path.join(os.path.dirname(args.model_ckpt),'{}_evalai_beam_{}_short_eval_{}_share2_{}.json'.
+                                       format(split, args.beam_size, args.short_eval, args.use_share2))
 
             # EvalAI/ST-VQA file
             answer_dict = []
@@ -575,15 +599,14 @@ def main():
                     'question_id': pred['question_id'],
                     'answer': pred['pred_answer'].strip()
                 })
-
             with open(evalai_file, 'w') as f:
                 json.dump(answer_dict, f)
-
             print(f"Dumping file: {evalai_file}")
 
 if __name__ == "__main__":
-    try:
-        main()
-    finally:
-        import pdb
-        pdb.set_trace()
+    main()
+    # try:
+    #     main()
+    # finally:
+    #     import pdb
+    #     pdb.set_trace()

vilbert/datasets/textvqa_dataset.py
@@ -167,7 +167,7 @@ class TextVQADataset(Dataset):
 
         if self.heads_type != "none":
             cache_path = cache_path.split(".")[0]
-            cache_path = cache_path + f"_heads_new" + ".pkl"
+            cache_path = cache_path + f"_heads_new_share5" + ".pkl"
 
         if self.randomize > 0:
             cache_path = cache_path.split(".")[0]
@@ -222,10 +222,12 @@ class TextVQADataset(Dataset):
         sp_pool.join()
         logger.info(f"Done Processsing Quadrant Spatial Relations with {self.processing_threads} threads")
         assert len(result_list) == len(mp_input)
-        for entry, (quad4, share3_1, share3_2) in zip(self.entries, result_list):
+        for entry, (quad4, share3_1, share3_2, share5_1, share5_2) in zip(self.entries, result_list):
             entry["spatial_adj_matrix_quad4"] = quad4
             entry["spatial_adj_matrix_share3_1"] = share3_1
             entry["spatial_adj_matrix_share3_2"] = share3_2
+            entry["spatial_adj_matrix_share5_1"] = share5_1
+            entry["spatial_adj_matrix_share5_2"] = share5_2
 
     def process(self):
         # Fill the boxes from readers
@@ -321,30 +323,47 @@ class TextVQADataset(Dataset):
             replace_dict[quad] = quad + 1
             replace_dict[quad + 1] = quad
 
-        spatial_adj_matrix_share3_1_replace_dict = {}
+        next_replace_dict = {}
         for quad in [4, 5, 6, 7, 8, 9, 10]:
-            spatial_adj_matrix_share3_1_replace_dict[quad] = quad + 1
-        spatial_adj_matrix_share3_1_replace_dict[11] = 4
+            next_replace_dict[quad] = quad + 1
+        next_replace_dict[11] = 4
 
-        spatial_adj_matrix_share3_2_replace_dict = {}
+        prev_replace_dict = {}
         for quad in [5, 6, 7, 8, 9, 10, 11]:
-            spatial_adj_matrix_share3_2_replace_dict[quad] = quad - 1
-        spatial_adj_matrix_share3_2_replace_dict[4] = 11
+            prev_replace_dict[quad] = quad - 1
+        prev_replace_dict[4] = 11
 
         spatial_adj_matrix_quad4 = np.copy(spatial_adj_matrix) * 0
         spatial_adj_matrix_share3_1 = np.copy(spatial_adj_matrix) * 0
         spatial_adj_matrix_share3_2 = np.copy(spatial_adj_matrix) * 0
+
+        spatial_adj_matrix_share5_1 = np.copy(spatial_adj_matrix) * 0
+        spatial_adj_matrix_share5_2 = np.copy(spatial_adj_matrix) * 0
 
         assert len(spatial_adj_matrix.shape) == 2
         rows, cols = spatial_adj_matrix.shape
         for row in range(rows):
             for col in range(cols):
                 spatial_adj_matrix_quad4[row][col] = replace_dict.get(spatial_adj_matrix[row][col], 0)
-                spatial_adj_matrix_share3_1[row][col] = spatial_adj_matrix_share3_1_replace_dict.get(
+
+                spatial_adj_matrix_share3_1[row][col] = next_replace_dict.get(
                     spatial_adj_matrix[row][col], 0)
-                spatial_adj_matrix_share3_2[row][col] = spatial_adj_matrix_share3_2_replace_dict.get(
+
+                spatial_adj_matrix_share3_2[row][col] = prev_replace_dict.get(
                     spatial_adj_matrix[row][col], 0)
-        return spatial_adj_matrix_quad4, spatial_adj_matrix_share3_1, spatial_adj_matrix_share3_2
+
+                spatial_adj_matrix_share5_1[row][col] = next_replace_dict.get(
+                    spatial_adj_matrix_share3_1[row][col], 0)
+
+                spatial_adj_matrix_share5_2[row][col] = prev_replace_dict.get(
+                    spatial_adj_matrix_share3_2[row][col], 0)
+
+        return (spatial_adj_matrix_quad4,
+                spatial_adj_matrix_share3_1,
+                spatial_adj_matrix_share3_2,
+                spatial_adj_matrix_share5_1,
+                spatial_adj_matrix_share5_2
+                )
 
     def __len__(self):
         return len(self.entries)
@@ -430,7 +449,9 @@ class TextVQADataset(Dataset):
                 "context_tokens": cleaned_ocr_tokens,
             })
             entry.update(processed_answers)
-
+        else:
+            # Empty placeholder
+            entry["train_prev_inds"] = torch.zeros(12, dtype=torch.long)
 
         if self.heads_type in ["mix", "share3", "quad4"] or self.randomize > 0 or "spatial_adj_matrix" in entry:
             # In the first iteration expand all the spatial relation matrices
@@ -465,25 +486,25 @@ class TextVQADataset(Dataset):
                         label_num=12
                     )
 
-                if self.heads_type == "quad4":
-                    spatial_adj_matrix_quad4 = torch_broadcast_adj_matrix(
-                        torch.from_numpy(entry["spatial_adj_matrix_quad4"]),
-                        label_num=12
-                    )
-                    entry["spatial_adj_matrix"] = torch.max(entry["spatial_adj_matrix"], spatial_adj_matrix_quad4)
-
-                if self.heads_type == "share3":
-                    spatial_adj_matrix_share3_1 = torch_broadcast_adj_matrix(
-                        torch.from_numpy(entry["spatial_adj_matrix_share3_1"]),
-                        label_num=12
-                    )
-
-                    spatial_adj_matrix_share3_2 = torch_broadcast_adj_matrix(
-                        torch.from_numpy(entry["spatial_adj_matrix_share3_2"]),
-                        label_num=12
-                    )
-                    entry["spatial_adj_matrix"] = torch.max(entry["spatial_adj_matrix"], spatial_adj_matrix_share3_1)
-                    entry["spatial_adj_matrix"] = torch.max(entry["spatial_adj_matrix"], spatial_adj_matrix_share3_2)
+                # if self.heads_type == "quad4":
+                #     spatial_adj_matrix_quad4 = torch_broadcast_adj_matrix(
+                #         torch.from_numpy(entry["spatial_adj_matrix_quad4"]),
+                #         label_num=12
+                #     )
+                #     entry["spatial_adj_matrix"] = torch.max(entry["spatial_adj_matrix"], spatial_adj_matrix_quad4)
+                #
+                # if self.heads_type == "share3":
+                #     spatial_adj_matrix_share3_1 = torch_broadcast_adj_matrix(
+                #         torch.from_numpy(entry["spatial_adj_matrix_share3_1"]),
+                #         label_num=12
+                #     )
+                #
+                #     spatial_adj_matrix_share3_2 = torch_broadcast_adj_matrix(
+                #         torch.from_numpy(entry["spatial_adj_matrix_share3_2"]),
+                #         label_num=12
+                #     )
+                #     entry["spatial_adj_matrix"] = torch.max(entry["spatial_adj_matrix"], spatial_adj_matrix_share3_1)
+                #     entry["spatial_adj_matrix"] = torch.max(entry["spatial_adj_matrix"], spatial_adj_matrix_share3_2)
 
                 if self.heads_type == "mix":
                     spatial_adj_matrix_quad4 = torch_broadcast_adj_matrix(
@@ -500,10 +521,28 @@ class TextVQADataset(Dataset):
                         torch.from_numpy(entry["spatial_adj_matrix_share3_2"]),
                         label_num=12
                     )
+
                     entry["spatial_adj_matrix_share3"] = torch.max(entry["spatial_adj_matrix"],
                                                                    spatial_adj_matrix_share3_1)
-                    entry["spatial_adj_matrix_share3"] = torch.max(entry["spatial_adj_matrix"],
+                    entry["spatial_adj_matrix_share3"] = torch.max(entry["spatial_adj_matrix_share3"],
                                                                    spatial_adj_matrix_share3_2)
+
+                    if registry.args["use_share2"]:
+                        entry["spatial_adj_matrix_share3"] = torch.max(entry["spatial_adj_matrix"],
+                                                                       spatial_adj_matrix_share3_2)
+
+                    spatial_adj_matrix_share5_1 = torch_broadcast_adj_matrix(
+                        torch.from_numpy(entry["spatial_adj_matrix_share5_1"]),
+                        label_num=12
+                    )
+                    spatial_adj_matrix_share5_2 = torch_broadcast_adj_matrix(
+                        torch.from_numpy(entry["spatial_adj_matrix_share5_2"]),
+                        label_num=12
+                    )
+                    entry["spatial_adj_matrix_share5"] = torch.max(entry["spatial_adj_matrix_share3"],
+                                                                   spatial_adj_matrix_share5_1)
+                    entry["spatial_adj_matrix_share5"] = torch.max(entry["spatial_adj_matrix_share5"],
+                                                                   spatial_adj_matrix_share5_2)
             else:
                 try:
                     assert len(entry["spatial_adj_matrix"].shape) == 3
@@ -517,6 +556,8 @@ class TextVQADataset(Dataset):
         # remove unwanted keys
         unwanted_keys = ['spatial_adj_matrix_share3_1',
                          'spatial_adj_matrix_share3_2',
+                         'spatial_adj_matrix_share5_1',
+                         'spatial_adj_matrix_share5_2',
                          'cleaned_ocr_tokens',
                          'image_id',
                          'image_path']

vilbert/m4c_regat.py
@@ -444,7 +444,7 @@ class SpatialBertSelfAttention(nn.Module):
     Todo: Keep 768 and build zero-mask for 12th head (not needed with identity relation)
     """
 
-    def __init__(self, config, use_implicit=False):
+    def __init__(self, config, use_implicit=False, use_regat=False):
         super(SpatialBertSelfAttention, self).__init__()
         assert hasattr(config, "num_spatial_relations")
 
@@ -484,6 +484,12 @@ class SpatialBertSelfAttention(nn.Module):
             self.use_bias = True
             logger.info("using head biases")
             self.biases = torch.nn.Embedding(1, config.hidden_size)
+
+        self.use_regat = use_regat
+        if self.use_regat:
+            self.regat_attention_bias = nn.Embedding(12, 1)
+            self.regat_value_bias = nn.Embedding(12, config.hidden_size)
+
 
     def transpose_for_scores(self, x):
         new_x_shape = x.size()[:-1] + (self.num_attention_heads, self.attention_head_size)
@@ -556,9 +562,14 @@ class SpatialBertSelfAttention(nn.Module):
         attention_scores = torch.matmul(query_layer, key_layer.transpose(-1, -2))
         attention_scores = attention_scores / math.sqrt(self.attention_head_size)
 
-        # Prevent imbalanced masking
-        combined_mask = torch.min(attention_mask, spatial_attention_mask)
-        assert len(torch.unique(combined_mask)) == 2
+        if not self.use_regat:
+            # Prevent imbalanced masking
+            combined_mask = torch.min(attention_mask, spatial_attention_mask)
+            assert len(torch.unique(combined_mask)) == 2
+        else:
+            # no spatial masking
+            combined_mask = attention_mask
+
 
         # for entities that are totally masked
         entity_probs_mask = (combined_mask.max(dim=-1)[0] + 10000.0)/10000.0
@@ -566,6 +577,13 @@ class SpatialBertSelfAttention(nn.Module):
 
         # Apply the attention mask is (precomputed for all layers in BertModel forward() function)
         attention_scores = attention_scores + combined_mask
+
+        if self.use_regat:
+            adj_matrix = spatial_adj_matrix.argmax(dim=-1) * spatial_adj_matrix.sum(dim=-1)
+            attention_bias = self.regat_attention_bias(adj_matrix)
+            attention_scores[:, self.max_seq_len:self.max_seq_len + ocr_obj_num,
+                self.max_seq_len:self.max_seq_len + ocr_obj_num, :] += attention_bias
+
 
         # Normalize the attention scores to probabilities.
         attention_probs = nn.Softmax(dim=-1)(attention_scores)
@@ -581,7 +599,15 @@ class SpatialBertSelfAttention(nn.Module):
         if head_mask is not None:
             attention_probs = attention_probs * head_mask
 
+        if self.use_regat:
+            # Modify value_layer
+            import pdb
+            pdb.set_trace()
+
         context_layer = torch.matmul(attention_probs, value_layer)
+
+
+
 
         context_layer = context_layer.permute(0, 2, 1, 3).contiguous()
         new_context_layer_shape = context_layer.size()[:-2] + (self.all_head_size,)
@@ -595,9 +621,9 @@ class SpatialBertSelfAttention(nn.Module):
 
 
 class SpatialBertAttention(nn.Module):
-    def __init__(self, config, use_implicit=False):
+    def __init__(self, config, use_implicit=False, use_regat):
         super(SpatialBertAttention, self).__init__()
-        self.self = SpatialBertSelfAttention(config, use_implicit)
+        self.self = SpatialBertSelfAttention(config, use_implicit, use_regat)
         from pytorch_transformers.modeling_bert import BertSelfOutput
         self.output = BertSelfOutput(config)
         self.pruned_heads = set()
@@ -633,10 +659,10 @@ class SpatialBertAttention(nn.Module):
 
 
 class SpatialBertLayer(nn.Module):
-    def __init__(self, config, use_implicit=False):
+    def __init__(self, config, use_implicit=False, use_regat=False):
         super(SpatialBertLayer, self).__init__()
         from pytorch_transformers.modeling_bert import BertIntermediate, BertOutput
-        self.attention = SpatialBertAttention(config, use_implicit)
+        self.attention = SpatialBertAttention(config, use_implicit, use_regat)
         self.intermediate = BertIntermediate(config)
         self.output = BertOutput(config)
 
@@ -682,10 +708,12 @@ class BertSpatialEncoder(nn.Module):
         self.num_spatial_layers = counter["s"]
         self.num_normal_layers = counter["n"]
         self.num_implicit_layers = counter["i"]
+        self.num_regat_layers = counter["r"]
 
         logger.info(f"Num Spatial Layers: {self.num_spatial_layers}")
         logger.info(f"Num Normal Layers: {self.num_normal_layers}")
         logger.info(f"Num Implicit Layers: {self.num_implicit_layers}")
+        logger.info(f"Num Implicit Layers: {self.num_regat_layers}")
 
         if config.mix_list is None:
             self.mix_list = ["none"]*len(self.layer_type_list)
@@ -697,6 +725,7 @@ class BertSpatialEncoder(nn.Module):
         self.normal_layers = nn.ModuleList([BertLayer(config) for _ in range(self.num_normal_layers)])
         self.spatial_layers = nn.ModuleList([SpatialBertLayer(config) for _ in range(self.num_spatial_layers)])
         self.implicit_layers = nn.ModuleList([SpatialBertLayer(config, True) for _ in range(self.num_implicit_layers)])
+        self.regat_layers = nn.ModuleList([SpatialBertLayer(config, False, True) for _ in range(self.num_regat_layers)])
 
     def forward(self, hidden_states, attention_mask, batch_dict, head_mask=None):
         all_hidden_states = ()
@@ -705,6 +734,7 @@ class BertSpatialEncoder(nn.Module):
         normal_iter = iter(self.normal_layers)
         spatial_iter = iter(self.spatial_layers)
         implicit_iter = iter(self.implicit_layers)
+        regat_iter = iter(self.regat_layers)
 
         for layer_type, mix_type in zip(self.layer_type_list, self.mix_list):
             if self.output_hidden_states:
@@ -714,6 +744,17 @@ class BertSpatialEncoder(nn.Module):
                 layer_outputs = layer_module(hidden_states, attention_mask)
             elif layer_type == "s":
                 layer_module = next(spatial_iter)
+                if mix_type == "share3":
+                    spatial_adj_matrix = batch_dict["spatial_adj_matrix_share3"]
+                elif mix_type == "quad4":
+                    spatial_adj_matrix = batch_dict["spatial_adj_matrix_quad4"]
+                elif mix_type == "share5":
+                    spatial_adj_matrix = batch_dict["spatial_adj_matrix_share5"]
+                else:
+                    spatial_adj_matrix = batch_dict["spatial_adj_matrix"]
+                layer_outputs = layer_module(hidden_states, attention_mask, spatial_adj_matrix)
+            elif layer_type == "r":
+                layer_module = next(regat_iter)
                 if mix_type == "share3":
                     spatial_adj_matrix = batch_dict["spatial_adj_matrix_share3"]
                 elif mix_type == "quad4":

vilbert/m4c_topk_20x.py
@@ -594,6 +594,82 @@ class SpatialBertSelfAttention(nn.Module):
         return outputs
 
 
+class TopkBertSelfAttention(nn.Module):
+    """
+    Todo: Keep 768 and build zero-mask for 12th head (not needed with identity relation)
+    """
+
+    def __init__(self, config):
+        super(TopkBertSelfAttention, self).__init__()
+        if config.hidden_size % config.num_attention_heads != 0:
+            raise ValueError(
+                "The hidden size (%d) is not a multiple of the number of attention "
+                "heads (%d)" % (config.hidden_size, config.num_attention_heads))
+        self.output_attentions = config.output_attentions
+
+        self.num_attention_heads = config.num_attention_heads
+        self.attention_head_size = int(config.hidden_size / config.num_attention_heads)
+        self.all_head_size = self.num_attention_heads * self.attention_head_size
+
+        self.query = nn.Linear(config.hidden_size, self.all_head_size)
+        self.key = nn.Linear(config.hidden_size, self.all_head_size)
+        self.value = nn.Linear(config.hidden_size, self.all_head_size)
+
+        self.dropout = nn.Dropout(config.attention_probs_dropout_prob)
+
+    def transpose_for_scores(self, x):
+        new_x_shape = x.size()[:-1] + (self.num_attention_heads, self.attention_head_size)
+        x = x.view(*new_x_shape)
+        return x.permute(0, 2, 1, 3)
+
+    def forward(self, hidden_states, attention_mask, top_k, head_mask=None):
+        mixed_query_layer = self.query(hidden_states)
+        mixed_key_layer = self.key(hidden_states)
+        mixed_value_layer = self.value(hidden_states)
+
+        query_layer = self.transpose_for_scores(mixed_query_layer)
+        key_layer = self.transpose_for_scores(mixed_key_layer)
+        value_layer = self.transpose_for_scores(mixed_value_layer)
+
+        # Take the dot product between "query" and "key" to get the raw attention scores.
+        attention_scores = torch.matmul(query_layer, key_layer.transpose(-1, -2))
+        attention_scores = attention_scores / math.sqrt(self.attention_head_size)
+
+        if top_k > 0:
+            obj_ocr_attention_scores = attention_scores[:, :, 20:170, 20:170]
+            _, top_indices = torch.topk(obj_ocr_attention_scores, top_k, dim=-1)
+            obj_ocr_top_k_mask = torch.zeros_like(obj_ocr_attention_scores, dtype=attention_mask.dtype) + (-10000.0)
+            obj_ocr_top_k_mask = obj_ocr_top_k_mask.scatter_(-1, top_indices, 0)
+            full_top_k_mask = torch.zeros_like(attention_scores, dtype=attention_mask.dtype)
+            full_top_k_mask[:, :, 20:170, 20:170] = full_top_k_mask[:, :, 20:170, 20:170] + obj_ocr_top_k_mask
+            # Prevent imbalanced masking
+            attention_mask = torch.min(attention_mask, full_top_k_mask)
+            assert len(torch.unique(attention_mask)) == 2
+
+        # Apply the attention mask is (precomputed for all layers in BertModel forward() function)
+        attention_scores = attention_scores + attention_mask
+
+        # Normalize the attention scores to probabilities.
+        attention_probs = nn.Softmax(dim=-1)(attention_scores)
+
+        # This is actually dropping out entire tokens to attend to, which might
+        # seem a bit unusual, but is taken from the original Transformer paper.
+        attention_probs = self.dropout(attention_probs)
+
+        # Mask heads if we want to
+        if head_mask is not None:
+            attention_probs = attention_probs * head_mask
+
+        context_layer = torch.matmul(attention_probs, value_layer)
+
+        context_layer = context_layer.permute(0, 2, 1, 3).contiguous()
+        new_context_layer_shape = context_layer.size()[:-2] + (self.all_head_size,)
+        context_layer = context_layer.view(*new_context_layer_shape)
+
+        outputs = (context_layer, attention_probs) if self.output_attentions else (context_layer,)
+        return outputs
+
+
 class SpatialBertAttention(nn.Module):
     def __init__(self, config, use_implicit=False):
         super(SpatialBertAttention, self).__init__()
@@ -631,6 +707,43 @@ class SpatialBertAttention(nn.Module):
         outputs = (attention_output,) + self_outputs[1:]  # add attentions if we output them
         return outputs
 
+class TopkBertAttention(nn.Module):
+    def __init__(self, config):
+        super(TopkBertAttention, self).__init__()
+        self.self = TopkBertSelfAttention(config)
+        from pytorch_transformers.modeling_bert import BertSelfOutput
+        self.output = BertSelfOutput(config)
+        self.pruned_heads = set()
+
+    def prune_heads(self, heads):
+        if len(heads) == 0:
+            return
+        mask = torch.ones(self.self.num_attention_heads, self.self.attention_head_size)
+        heads = set(heads) - self.pruned_heads  # Convert to set and emove already pruned heads
+        for head in heads:
+            # Compute how many pruned heads are before the head and move the index accordingly
+            head = head - sum(1 if h < head else 0 for h in self.pruned_heads)
+            mask[head] = 0
+        mask = mask.view(-1).contiguous().eq(1)
+        index = torch.arange(len(mask))[mask].long()
+
+        # Prune linear layers
+        self.self.query = prune_linear_layer(self.self.query, index)
+        self.self.key = prune_linear_layer(self.self.key, index)
+        self.self.value = prune_linear_layer(self.self.value, index)
+        self.output.dense = prune_linear_layer(self.output.dense, index, dim=1)
+
+        # Update hyper params and store pruned heads
+        self.self.num_attention_heads = self.self.num_attention_heads - len(heads)
+        self.self.all_head_size = self.self.attention_head_size * self.self.num_attention_heads
+        self.pruned_heads = self.pruned_heads.union(heads)
+
+    def forward(self, input_tensor, attention_mask, top_k, head_mask=None):
+        self_outputs = self.self(input_tensor, attention_mask, top_k, head_mask)
+        attention_output = self.output(self_outputs[0], input_tensor)
+        outputs = (attention_output,) + self_outputs[1:]  # add attentions if we output them
+        return outputs
+
 
 class SpatialBertLayer(nn.Module):
     def __init__(self, config, use_implicit=False):
@@ -650,6 +763,24 @@ class SpatialBertLayer(nn.Module):
         outputs = (layer_output,) + attention_outputs[1:]  # add attentions if we output them
         return outputs
 
+class TopkBertLayer(nn.Module):
+    def __init__(self, config):
+        super(TopkBertLayer, self).__init__()
+        from pytorch_transformers.modeling_bert import BertIntermediate, BertOutput
+        self.attention = TopkBertAttention(config)
+        self.intermediate = BertIntermediate(config)
+        self.output = BertOutput(config)
+
+    def forward(self, hidden_states, attention_mask, top_k, head_mask=None):
+        attention_outputs = self.attention(hidden_states, attention_mask, top_k, head_mask)
+        attention_output = attention_outputs[0]
+        # Intermediate is dense + activation
+        intermediate_output = self.intermediate(attention_output)
+        # Output is dense + dropout + residual + layernorm
+        layer_output = self.output(intermediate_output, attention_output)
+        outputs = (layer_output,) + attention_outputs[1:]  # add attentions if we output them
+        return outputs
+
 
 class BertSpatialEncoder(nn.Module):
     def __init__(self, config):
@@ -661,6 +792,7 @@ class BertSpatialEncoder(nn.Module):
 
         # backward compatibility
         if config.layer_type_list is None:
+            raise ValueError
             logger.info("layer_type_list not passed, generating it!")
             self.layer_type_list = ["n"]*config.num_hidden_layers
 
@@ -682,10 +814,14 @@ class BertSpatialEncoder(nn.Module):
         self.num_spatial_layers = counter["s"]
         self.num_normal_layers = counter["n"]
         self.num_implicit_layers = counter["i"]
+        self.top1_layers = counter["t1"]
+        self.top3_layers = counter["t3"]
 
         logger.info(f"Num Spatial Layers: {self.num_spatial_layers}")
         logger.info(f"Num Normal Layers: {self.num_normal_layers}")
         logger.info(f"Num Implicit Layers: {self.num_implicit_layers}")
+        logger.info(f"Num Top1 Layers: {self.top1_layers}")
+        logger.info(f"Num Top3 Layers: {self.top3_layers}")
 
         if config.mix_list is None:
             self.mix_list = ["none"]*len(self.layer_type_list)
@@ -697,6 +833,8 @@ class BertSpatialEncoder(nn.Module):
         self.normal_layers = nn.ModuleList([BertLayer(config) for _ in range(self.num_normal_layers)])
         self.spatial_layers = nn.ModuleList([SpatialBertLayer(config) for _ in range(self.num_spatial_layers)])
         self.implicit_layers = nn.ModuleList([SpatialBertLayer(config, True) for _ in range(self.num_implicit_layers)])
+        self.top1_layers = nn.ModuleList([TopkBertLayer(config) for _ in range(self.top1_layers)])
+        self.top3_layers = nn.ModuleList([TopkBertLayer(config) for _ in range(self.top3_layers)])
 
     def forward(self, hidden_states, attention_mask, batch_dict, head_mask=None):
         all_hidden_states = ()
@@ -705,6 +843,8 @@ class BertSpatialEncoder(nn.Module):
         normal_iter = iter(self.normal_layers)
         spatial_iter = iter(self.spatial_layers)
         implicit_iter = iter(self.implicit_layers)
+        top1_iter = iter(self.top1_layers)
+        top3_iter = iter(self.top3_layers)
 
         for layer_type, mix_type in zip(self.layer_type_list, self.mix_list):
             if self.output_hidden_states:
@@ -718,8 +858,6 @@ class BertSpatialEncoder(nn.Module):
                     spatial_adj_matrix = batch_dict["spatial_adj_matrix_share3"]
                 elif mix_type == "quad4":
                     spatial_adj_matrix = batch_dict["spatial_adj_matrix_quad4"]
-                elif mix_type == "share5":
-                    spatial_adj_matrix = batch_dict["spatial_adj_matrix_share5"]
                 else:
                     spatial_adj_matrix = batch_dict["spatial_adj_matrix"]
                 layer_outputs = layer_module(hidden_states, attention_mask, spatial_adj_matrix)
@@ -727,13 +865,17 @@ class BertSpatialEncoder(nn.Module):
                 layer_module = next(implicit_iter)
                 if mix_type == "share3":
                     spatial_adj_matrix = batch_dict["spatial_adj_matrix_share3"]
-                elif mix_type == "share5":
-                    spatial_adj_matrix = batch_dict["spatial_adj_matrix_share5"]
                 elif mix_type == "quad4":
                     spatial_adj_matrix = batch_dict["spatial_adj_matrix_quad4"]
                 else:
                     spatial_adj_matrix = batch_dict["spatial_adj_matrix"]
                 layer_outputs = layer_module(hidden_states, attention_mask, spatial_adj_matrix)
+            elif layer_type == "t1":
+                layer_module = next(top1_iter)
+                layer_outputs = layer_module(hidden_states, attention_mask, 20)
+            elif layer_type == "t3":
+                layer_module = next(top3_iter)
+                layer_outputs = layer_module(hidden_states, attention_mask, 60)
             else:
                 raise ValueError
             hidden_states = layer_outputs[0]